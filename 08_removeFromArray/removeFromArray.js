3c5389f1406b4dfc91dff15fa988d24c6173ae86
Andy Hall <andersonhall@gmail.com>
2025-05-09 07:56:16 -0500

    08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+  // create a result set
+  const newArr = [];
+  // loop through args
+  arr.forEach((arg) => {
+    if (!args.includes(arg)) {
+      newArr.push(arg);
+    }
+  });
+  return newArr;
 };
 
 // Do not edit below this line
