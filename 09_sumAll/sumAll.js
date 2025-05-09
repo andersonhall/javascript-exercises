7bd645aa19ee79fd35dbc7f785abc477cb064a70
Andy Hall <andersonhall@gmail.com>
2025-05-09 08:08:32 -0500

    09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
-
+const sumAll = function (int1, int2) {
+  if (
+    int1 < 0 ||
+    int2 < 0 ||
+    !Number.isInteger(int1) ||
+    !Number.isInteger(int2) ||
+    isNaN(int1) ||
+    isNaN(int2)
+  ) {
+    return "ERROR";
+  }
+  let sum = 0;
+  if (int1 < int2) {
+    let baseValue = int1;
+    while (baseValue <= int2) {
+      sum += baseValue;
+      baseValue++;
+    }
+  } else {
+    let baseValue = int2;
+    while (baseValue <= int1) {
+      sum += baseValue;
+      baseValue++;
+    }
+  }
+  return sum;
 };
 
 // Do not edit below this line
