af9b365ea3f1fb0d514326fc94651e97af22c5b8
Andy Hall <andersonhall@gmail.com>
2025-05-09 22:45:59 -0500

    14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+  if (parseInt(n) === 0) {
+    return 0;
+  } else if (parseInt(n) < 0) {
+    return "OOPS";
+  } else {
+    let current = 1;
+    let next = 1;
+    let temp;
+    for (let i = 0; i < parseInt(n); i++) {
+      temp = current;
+      current = next;
+      next = temp + next;
+    }
+    return parseInt(temp);
+  }
 };
 
 // Do not edit below this line
