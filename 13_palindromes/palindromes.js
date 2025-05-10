19048c878443b814b4534e203a3e019ed758c5d7
Andy Hall <andersonhall@gmail.com>
2025-05-09 22:31:39 -0500

    13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
+const palindromes = function (word) {
+  const validChars = "abcdefghijklmnopqrstuvwxyz0123456789";
 
+  const newStr = word
+    .toLowerCase()
+    .split("")
+    .filter((character) => validChars.includes(character))
+    .join("");
+
+  const revStr = newStr.split("").reverse().join("");
+
+  // Return the outcome of the comparison which will either be true or false
+  return newStr === revStr;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
